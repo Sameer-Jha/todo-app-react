d16f8fff6b172b7bb35178f1a4b47c9f45a0e4b8
Sameer-Jha <sameer.btech.cs17@iiitranchi.ac.in>
2020-08-29 02:28:08 +0530

    Updates

---
+++ precache-manifest.dba0accfadbd6e61c6a2f841daeb6c19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe477fc0375a43ec5df78b492f0febf",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "6d1427b4947c69e667e9",
+    "url": "/todo-app-react/static/css/main.7582e2bd.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/todo-app-react/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1427b4947c69e667e9",
+    "url": "/todo-app-react/static/js/main.e12e325c.chunk.js"
+  },
+  {
+    "revision": "66794f4cb98817f22549",
+    "url": "/todo-app-react/static/js/runtime-main.716bd660.js"
+  }
+]);
